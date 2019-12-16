aeb05b1e4540928e46280210267ebf3a07bc3ca6
njanirudh <anijaya9@gmail.com>
2019-12-17 00:25:52 +0100

    Main function

--- src/main.py
+++ src/main.py
@@ -1,11 +1,18 @@
-from cv_object_detector import CVTFObjectDetector
+from nut_detector import NutDetector
 import argparse
 
 if __name__=="__main__":
-    VIDEO = "/home/nj/HBRS/Studies/Sem-3/CV/Dataset/Videos/CV19_video_219.avi"
-    Result_Path = "/home/nj/Desktop/result.csv"
 
-    nut_detector = NutDetector(VIDEO,Result_Path)
-    nut_detector.extract_most_stable_frame()
-    nut_detector.run_detection()
-    nut_detector.get_results()
+    parser = argparse.ArgumentParser(description='Commandline application to detect Nuts in a video.')
+    parser.add_argument('--video_path',
+                        help='path of the video to run detection')
+    parser.add_argument('--result_path',
+                        help='path of the folder to save evaluation results.')
+
+    args = parser.parse_args()
+    print(args)
+
+    # nut_detector = NutDetector(VIDEO,Result_Path)
+    # nut_detector.extract_most_stable_frame()
+    # nut_detector.run_detection()
+    # nut_detector.get_results()
