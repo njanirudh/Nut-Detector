8cad6006a11f16bf8957e757c951c6214901ada0
Anirudh <anijaya9@gmail.com>
2020-01-09 21:10:12 +0100

    Minor orrected error

--- src/main.py
+++ src/main.py
@@ -7,12 +7,12 @@ from utility import *
 from nut_detector import NutDetector
 
 # st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-# sys.stdout = open(", 'r')
+# sys.stdout = open("../log.txt", 'r')
 
 if __name__=="__main__":
 
     parser = argparse.ArgumentParser(description='Commandline application to detect Nuts in a video.')
-    parser.add_argument('--video_path', type=str, default='../',
+    parser.add_argument('--video_path', type=str, default='..\\test_video.mp4',
                         help='path of the video to run detection')
     parser.add_argument('--result_path', type=str, default='..\Results',
                         help='path to the folder to save evaluation results.')
@@ -32,16 +32,19 @@ if __name__=="__main__":
         print(INFO("Video found successfully."))
     else:
         print(ERROR("Video file not valid."))
+        sys.exit()
     # ------
     if Path(args.frozen_graph).is_file():
         print(INFO("Frozen graph found successfully."))
     else:
         print(ERROR("Frozen graph file not found."))
+        sys.exit()
     # ------
     if Path(args.pbtxt_path).is_file():
         print(INFO("Pbtxt found successfully."))
     else:
         print(ERROR("Pbtxt file not found."))
+        sys.exit()
 
     # -------------------------------------------------------
 
